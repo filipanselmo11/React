9b1d81802538a6c4b2c3821d9959eced856b8be0
Filip Anselmo <filip_anselmo@yahoo.com.br>
2021-03-13 09:26:29 -0400

    Adicionando novos componentes do formulário

---
+++ projeto-atual/src/components/FormularioCadastro/DadosUsuario.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { TextField, Button } from "@material-ui/core"
+
+export default function DadosUsuario({aoEnviar}) {
+    return(
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            aoEnviar();   
+        }}>
+            <TextField id="email" label="email" type="email" required variant="outlined" margin="normal" fullWidth/>
+            <TextField id="senha" label="senha" type="password" required variant="outlined" margin="normal" fullWidth/>
+            <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
+        </form>
+    );
+}
--- projeto-atual/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ projeto-atual/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,93 +1,43 @@
 import React, { useState } from "react";
-import { Button, FormControlLabel, Switch, TextField } from "@material-ui/core";
+import DadosEntrega from "./DadosEntrega";
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
+import { Typography } from '@material-ui/core'
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
-  const [nome, setNome] = useState("");
-  const [sobrenome, setSobrenome] = useState("");
-  const [cpf, setCpf] = useState("");
-  const [promocoes, setPromocoes] = useState(true);
-  const [novidades, setNovidades] = useState(false);
-  const [erros, setErros] = useState({ cpf: { valido: true, texto: "" } });
+  const [etapaAtual, setEtapaAtual] = useState(0)
 
+  function proximo() {
+    setEtapaAtual(etapaAtual + 1);
+  }
+
+  function formularioAtual(etapa) {
+    switch (etapaAtual) {
+      case 0:
+        return <DadosUsuario aoEnviar={proximo} />
+      case 1:
+        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />
+      case 2:
+        return <DadosEntrega aoEnviar={aoEnviar} />
+      default:
+        return <Typography>Erro ao selecionar Formulário</Typography>
+    }
+  }
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        aoEnviar({ nome, sobrenome, cpf, novidades, promocoes });
-      }}
-    >
-      <TextField
-        value={nome}
-        onChange={(event) => {
-          setNome(event.target.value);
-        }}
-        id="nome"
-        label="Nome"
-        variant="outlined"
-        margin="normal"
-        fullWidth
-      />
+    <>
+      {formularioAtual(etapaAtual)}
+    </>
 
-      <TextField
-        value={sobrenome}
-        onChange={(event) => {
-          setSobrenome(event.target.value);
-        }}
-        id="sobrenome"
-        label="Sobrenome"
-        variant="outlined"
-        margin="normal"
-        fullWidth
-      />
-
-      <TextField
-        value={cpf}
-        onChange={(event) => {
-          setCpf(event.target.value);
-        }}
-        onBlur={(event) => {
-          const ehValido = validarCPF(cpf);
-          setErros(ehValido);
-        }}
-        error={!erros.cpf.valido}
-        helperText={erros.cpf.texto}
-        id="cpf"
-        label="CPF"
-        variant="outlined"
-        margin="normal"
-        fullWidth
-      />
-      <FormControlLabel
-        label="Promoções"
-        control={
-          <Switch
-            checked={promocoes}
-            onChange={(event) => {
-              setPromocoes(event.target.checked);
-            }}
-            name="promocoes"
-            color="primary"
-          />
-        }
-      />
-      <FormControlLabel
-        label="Novidades"
-        control={
-          <Switch
-            checked={novidades}
-            onChange={(event) => {
-              setNovidades(event.target.checked);
-            }}
-            name="novidade"
-            color="primary"
-          />
-        }
-      />
-
-      <Button variant="contained" color="primary" type="submit">
-        Cadastrar
-      </Button>
-    </form>
-  );
+  )
 }
+
+
+
+
+//
+//
+//
+//
+//
+
 export default FormularioCadastro;
