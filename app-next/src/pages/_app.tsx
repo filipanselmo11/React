c466bbd3aa23a68a1b4017eb64bb2607547d9617
Filip Anselmo <filip_anselmo@yahoo.com.br>
2021-03-13 08:17:25 -0400

    atualizando o repo

--- app-next/src/pages/_app.tsx
+++ app-next/src/pages/_app.tsx
@@ -4,9 +4,7 @@ import { } from '../contexts/ChallengeContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengeProvider>
-      <Component {...pageProps}/>
-    </ChallengeProvider>
+    <Component {...pageProps}/>
   );
 }
 
--- app-next/src/pages/index.tsx
+++ app-next/src/pages/index.tsx
@@ -1,64 +1,64 @@
-import Head from 'next/head'
-import styles from '../styles/pages/Home.module.css'
-export default function Home() {
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import Countdown from "../components/Countdown";
+import ExperienceBar from "../components/ExperienceBar";
+import Profile from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
+import styles from '../styles/pages/Home.module.css';
+import CountdownProvider from "../contexts/CountdownContext";
+import { ChallengeProvider } from "../contexts/ChallengeContext";
+
+interface HomeProps {
+  level: number,
+  currentExperience: number,
+  challengesCompleted: number
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <ChallengeProvider 
+      level={props.level} 
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}>
+        <div className={styles.container}>
+          <Head>
+            <title>Início | move.it</title>
+          </Head>
+          <ExperienceBar/>
+          <CountdownProvider>
+            <section>
+              <div>
+                <Profile/>
+                <CompletedChallenges/>
+                <Countdown/>
+              </div>
+              <div>
+                <ChallengeBox/>
+              </div>
+            </section>
+          </CountdownProvider>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      </ChallengeProvider>
   )
+}
+
+
+export const getServeSideProps: GetServerSideProps = async(ctx) => {
+  const user = {
+    level: 1,
+    currentExperience: 50,
+    challengesCompleted: 2,
+  }
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
